7a665bf9ce106b5b5d7b9fb22d6942c514d877d9
Loredana Sanduleac <70658391+LoriSanduleac04@users.noreply.github.com>
2020-12-06 01:01:41 +0200

    Add files via upload

---
+++ problemaolimpiada2.py
@@ -0,0 +1,4 @@
+n=int(input("introduceti nr de alune:"))
+p=int(n/3)
+r=n-(p*3)
+print("fiecare purcelus a primit",p, "alune, iar Rita a primit", r, "alune")
---
+++ problemaolimpiada3.py
@@ -0,0 +1,5 @@
+n=int(input("introduceti nr de ani:"))
+l=12*n
+z=365*n
+o=z*24
+print("in ",n," ani sunt ",l," luni ",z," zile ",o, " ore")
