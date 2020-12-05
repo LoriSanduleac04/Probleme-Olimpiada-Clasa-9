adc82d5a6d089eb400a90b0d3e9cb6873b4e7ecb
Loredana Sanduleac <70658391+LoriSanduleac04@users.noreply.github.com>
2020-12-06 01:15:13 +0200

    Add files via upload

---
+++ problemaolimpiada4.py
@@ -0,0 +1,13 @@
+#lungimea gardului patratului
+p=48
+#latimea dreptunghiului
+lm=9
+#latura patratului
+lp=48/4
+#aria 
+Ap=lp**2
+#latura dreptunghiului
+lt=Ap/lm
+#lungimea gardului dreptunghiului
+d=(lt+lp)*2
+print("lungimea gardului care inconjoara lotul celui de-al doilea vecin este de: ",d," metri")
